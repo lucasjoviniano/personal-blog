c111521d6f712d078d652d5a8c52487154229825
Lucas Joviniano <lucasjoviniano1@gmail.com>
2021-08-08 23:01:59 -0300

    Deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/personal-blog/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
