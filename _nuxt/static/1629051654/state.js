67f655206ff2331f2ce6a839e28a33826780e4d1
lucasjoviniano <lucasjoviniano@users.noreply.github.com>
2021-08-15 18:31:53 +0000

    deploy: 73162450dbbe123e021936d47ecaea56db52b1e8

--- _nuxt/static/1629051654/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629051654",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"553cb801"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1629052311/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629052311",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"39e13226"}}}}(null,"\u002F"));
